81c6a72ef322becd10009c89a6618df1f15cd632
gtafiper <onechr@gmail.com>
2020-05-21 11:48:01 +0200

    Update Program.cs

--- AnimuCrawler/Program.cs
+++ AnimuCrawler/Program.cs
@@ -9,14 +9,14 @@ namespace AnimuCrawler
     {
         static void Main(string[] args)
         {
-            /*Console.Write("Enter an URL: ");
+            Console.Write("Enter an URL: ");
             var urlStr = Console.ReadLine();
             Console.Write("Enter an Name: ");
             var nameStr = Console.ReadLine();
 
             AnimuCrawlerBot senpai = new AnimuCrawlerBot(urlStr, nameStr);
             senpai.StartWatching();
-            Console.ReadLine();*/
+            Console.ReadLine();
         }
     }
 }
